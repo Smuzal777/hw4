dc0208959b0d1175133a7d1664b1dd36e01b2c59
Софья Музалева <smuzal@yandex.ru>
2024-10-13 15:39:30 +0300

    Ваше сообщение

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
 ﻿begin
-  Print('Привет, мир!');
+  Println('Выберите язык, на котором говорите: 1 - русский, 2 - английский, 3 - немецкий, 4 - французский:');
+  var lan := ReadInteger('Введите цифру:');
+  case lan of
+    1: Println('Привет, мир!');
+    2: Println('Hello, world!');
+    3: Println('Hallo, Welt!');
+    4: Println('Bonjour, le monde!');
+  end;
 end.
